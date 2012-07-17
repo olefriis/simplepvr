c493f1c129d1840f103ef65c730d45bfdb043dc9
Ole Friis <olefriis@gmail.com>
2012-07-17 18:46:50 +0200

    Rearranged XMLTV parser, so that it pushes to a DAO

--- lib/simple_pvr/xmltv_reader.rb
+++ lib/simple_pvr/xmltv_reader.rb
@@ -1,41 +1,36 @@
 require "rexml/document"
 
 module SimplePvr
-  XmltvResult = Struct.new(:channels, :programmes)
-  XmltvChannel = Struct.new(:id, :name, :icon_url)
-  XmltvProgramme = Struct.new(:title, :subtitle, :description, :start_time, :duration)
-
   class XmltvReader
+    def initialize(dao, mapping_to_channels)
+      @dao, @mapping_to_channels = dao, mapping_to_channels
+    end
+    
     def read(input)
-      
+      @dao.clear_programmes
       doc = REXML::Document.new input
-      
-      XmltvResult.new(read_channels(doc), read_programmes(doc))
+
+      doc.elements.each('tv/programme') do |programme|
+        process_programme(programme)
+      end
     end
     
     private
-    def read_channels(doc)
-      result = []
-      doc.elements.each('tv/channel') do |channel|
-        result << XmltvChannel.new(channel.attributes['id'], channel.elements['display-name'].text, channel.elements['icon'].attributes['src'])
-      end
-      result
+    def process_programme(programme)
+      channel_id = programme.attributes['channel']
+      channel = @mapping_to_channels[channel_id]
+
+      add_programme(channel, programme) if channel
     end
     
-    def read_programmes(doc)
-      result = {}
-      doc.elements.each('tv/programme') do |programme|
-        channel_id = programme.attributes['channel']
-        title = programme.elements['title'].text
-        subtitle = programme.elements['sub-title'] ? programme.elements['sub-title'].text : ''
-        description = programme.elements['desc'].text
-        start_time = Time.parse(programme.attributes['start'])
-        stop_time = Time.parse(programme.attributes['stop'])
+    def add_programme(channel, programme)
+      title = programme.elements['title'].text
+      subtitle = programme.elements['sub-title'] ? programme.elements['sub-title'].text : ''
+      description = programme.elements['desc'] ? programme.elements['desc'].text : ''
+      start_time = Time.parse(programme.attributes['start'])
+      stop_time = Time.parse(programme.attributes['stop'])
 
-        result[channel_id] ||= []
-        result[channel_id] << XmltvProgramme.new(title, subtitle, description, start_time, stop_time - start_time)
-      end
-      result
+      @dao.add_programme(channel, title, subtitle, description, start_time, stop_time - start_time)
     end
   end
 end
--- spec/simple_pvr/xmltv_reader_spec.rb
+++ spec/simple_pvr/xmltv_reader_spec.rb
@@ -2,28 +2,42 @@
 require 'simple_pvr/xmltv_reader'
 
 describe SimplePvr::XmltvReader do
+  MockProgramme = Struct.new(:title, :subtitle, :description, :start_time, :duration)
+
+  class MockDao
+    attr :programmes
+
+    def clear_programmes
+      @programmes = {}
+    end
+    
+    def add_programme(channel, *arguments)
+      @programmes[channel] ||= []
+      @programmes[channel] << MockProgramme.new(*arguments)
+    end
+  end
+  
   before do
-    @xmltv_reader = SimplePvr::XmltvReader.new
+    @dao = MockDao.new
+    @xmltv_reader = SimplePvr::XmltvReader.new(@dao, {'www.ontv.dk/tv/1' => 'DR 1'})
   end
   
-  it 'reads the channel information' do
-    data = @xmltv_reader.read(File.new(File.dirname(__FILE__) + '/../resources/programs.xmltv'))
+  it 'populates programme information through the DAO' do
+    @xmltv_reader.read(File.new(File.dirname(__FILE__) + '/../resources/programs.xmltv'))
     
-    data.channels.length.should == 2
-    data.channels[0].id.should == 'www.ontv.dk/tv/1'
-    data.channels[0].name.should == 'DR1 DK'
-    data.channels[0].icon_url.should == 'http://ontv.dk/imgs/epg/logos/dr1_big.png'
-  end
-  
-  it 'reads programme information' do
-    data = @xmltv_reader.read(File.new(File.dirname(__FILE__) + '/../resources/programs.xmltv'))
-    
-    data.programmes['www.ontv.dk/tv/1'].length.should == 5
-    noddy = data.programmes['www.ontv.dk/tv/1'][0]
+    @dao.programmes['DR 1'].length.should == 5
+    noddy = @dao.programmes['DR 1'][0]
     noddy.title.should == 'Noddy'
     noddy.subtitle.should == 'Bare vær dig selv, Noddy.'
     noddy.description.should == "Tegnefilm.\nHer kommer Noddy - så kom ud og leg! Den lille dreng af træ har altid travlt med at køre sine venner rundt i Legebyen - og du kan altid høre, når han er på vej!"
     noddy.start_time.should == Time.new(2012, 7, 17, 6, 0, 0, "+02:00")
     noddy.duration.should == 10.minutes
+  end
+  
+  it 'ignores programmes for channels with no mapping' do
+    @xmltv_reader.read(File.new(File.dirname(__FILE__) + '/../resources/programs.xmltv'))
+
+    # There are two channels in the XMLTV file, but only one with a mapping
+    @dao.programmes.length.should == 1
   end
 end
