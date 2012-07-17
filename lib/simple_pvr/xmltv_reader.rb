dffa8a33ea2f72c297579ae1ccbeb90af80f5f2f
Ole Friis <olefriis@gmail.com>
2012-07-17 13:33:04 +0200

    Enough XMLTV parsing for now

--- lib/simple_pvr/xmltv_reader.rb
+++ lib/simple_pvr/xmltv_reader.rb
@@ -1,22 +1,39 @@
 require "rexml/document"
 
 module SimplePvr
-  XmltvResult = Struct.new(:channels)
+  XmltvResult = Struct.new(:channels, :programmes)
   XmltvChannel = Struct.new(:id, :name, :icon_url)
+  XmltvProgramme = Struct.new(:title, :subtitle, :description, :start_time, :duration)
 
   class XmltvReader
     def read(input)
       
       doc = REXML::Document.new input
       
-      XmltvResult.new(read_channels(doc))
+      XmltvResult.new(read_channels(doc), read_programmes(doc))
     end
     
     private
     def read_channels(doc)
       result = []
-      doc.elements.each("tv/channel") do |channel|
+      doc.elements.each('tv/channel') do |channel|
         result << XmltvChannel.new(channel.attributes['id'], channel.elements['display-name'].text, channel.elements['icon'].attributes['src'])
+      end
+      result
+    end
+    
+    def read_programmes(doc)
+      result = {}
+      doc.elements.each('tv/programme') do |programme|
+        channel_id = programme.attributes['channel']
+        title = programme.elements['title'].text
+        subtitle = programme.elements['sub-title'] ? programme.elements['sub-title'].text : ''
+        description = programme.elements['desc'].text
+        start_time = Time.parse(programme.attributes['start'])
+        stop_time = Time.parse(programme.attributes['stop'])
+
+        result[channel_id] ||= []
+        result[channel_id] << XmltvProgramme.new(title, subtitle, description, start_time, stop_time - start_time)
       end
       result
     end
--- spec/simple_pvr/xmltv_reader_spec.rb
+++ spec/simple_pvr/xmltv_reader_spec.rb
@@ -1,3 +1,4 @@
+#encoding: UTF-8
 require 'simple_pvr/xmltv_reader'
 
 describe SimplePvr::XmltvReader do
@@ -12,5 +13,17 @@ describe SimplePvr::XmltvReader do
     data.channels[0].id.should == 'www.ontv.dk/tv/1'
     data.channels[0].name.should == 'DR1 DK'
     data.channels[0].icon_url.should == 'http://ontv.dk/imgs/epg/logos/dr1_big.png'
+  end
+  
+  it 'reads programme information' do
+    data = @xmltv_reader.read(File.new(File.dirname(__FILE__) + '/../resources/programs.xmltv'))
+    
+    data.programmes['www.ontv.dk/tv/1'].length.should == 5
+    noddy = data.programmes['www.ontv.dk/tv/1'][0]
+    noddy.title.should == 'Noddy'
+    noddy.subtitle.should == 'Bare vær dig selv, Noddy.'
+    noddy.description.should == "Tegnefilm.\nHer kommer Noddy - så kom ud og leg! Den lille dreng af træ har altid travlt med at køre sine venner rundt i Legebyen - og du kan altid høre, når han er på vej!"
+    noddy.start_time.should == Time.new(2012, 7, 17, 6, 0, 0, "+02:00")
+    noddy.duration.should == 10.minutes
   end
 end
