6d39dc2767d59cf5fa73f5397f6a54765031d626
Ole Friis <olefriis@gmail.com>
2012-09-11 16:31:45 +0200

    Oh my god, secured server was broken...

--- lib/simple_pvr/server.rb
+++ lib/simple_pvr/server.rb
@@ -3,19 +3,19 @@ require 'sinatra/base'
 Time::DATE_FORMATS[:programme_date] = '%F'
 Time::DATE_FORMATS[:day] = '%a, %d %b'
 
-http_username, http_password = ENV['username'], ENV['password']
-if http_username && http_password
-  SimplePvr::PvrLogger.info('Securing server with Basic HTTP Authentication')
-  use Rack::Auth::Basic, 'Restricted Area' do |username, password|
-    [username, password] == [http_username, http_password]
-  end
-else
-  SimplePvr::PvrLogger.info('Beware: Unsecured server. Do not expose to the rest of the world!')
-end
-
 module SimplePvr
   class Server < Sinatra::Base
     include ERB::Util
+
+    http_username, http_password = ENV['username'], ENV['password']
+    if http_username && http_password
+      PvrLogger.info('Securing server with Basic HTTP Authentication')
+      use Rack::Auth::Basic, 'Restricted Area' do |username, password|
+        [username, password] == [http_username, http_password]
+      end
+    else
+      PvrLogger.info('Beware: Unsecured server. Do not expose to the rest of the world!')
+    end
 
     settings.public_folder = File.dirname(__FILE__) + '/../../public/'
 
