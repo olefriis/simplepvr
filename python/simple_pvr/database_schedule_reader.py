919859e62e6dd219e8abfdf2d7b85b985ab06fd6
Flemming Jønsson <flemming@joensson.org>
2012-10-03 16:10:19 +0200

    Clear schedule list before adding all scheduled programmes to it

--- python/simple_pvr/database_schedule_reader.py
+++ python/simple_pvr/database_schedule_reader.py
@@ -10,7 +10,8 @@ class DatabaseScheduleReader():
     def read(self):
         from .master_import import Schedule
 
+        self._recording_planner._recordings = []
         for schedule in db.session.query(Schedule).all():
-            self._recording_planner.specification(schedule.title, schedule.channel)
+            self._recording_planner.specification(schedule.title, schedule.channel, schedule.start_time)
         
         self._recording_planner.finish()
