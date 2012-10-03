ffbe07e93183032b413c8acde3530f42a9dda380
Flemming Jønsson <flemming@joensson.org>
2012-10-03 16:11:55 +0200

    Programme stop_time and episode number added

--- python/simple_pvr/programme.py
+++ python/simple_pvr/programme.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import Column, MetaData
 from sqlalchemy.schema import UniqueConstraint
 from sqlalchemy.orm import sessionmaker, relationship, backref
@@ -15,8 +15,6 @@ association_table = db.Table('programmes_categories',
 )
 
 class Programme(db.Model):
-
-
     __tablename__ = 'programmes'
 
     id = db.Column(Integer, primary_key=True)
@@ -24,8 +22,10 @@ class Programme(db.Model):
     subtitle = db.Column(String(255))
     description = db.Column(db.Text)
     series = db.Column(Boolean, nullable = False, default=False)
-    startTime = db.Column(db.DateTime)
+    start_time = db.Column(db.DateTime, index=True)
+    stop_time = db.Column(db.DateTime, index=True)
     duration = db.Column(Integer)
+    episode_num = db.Column(String(255), index=True)
 
     channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'))
     channel = db.relationship('Channel', primaryjoin="Programme.channel_id == Channel.id",
@@ -34,15 +34,15 @@ class Programme(db.Model):
     categories = db.relationship('Category', secondary=association_table,
                            backref=db.backref('programmes', lazy='dynamic'))
 
-    def __init__(self, channel, title, subtitle, description, start_time, duration, series=False, categories = []):
+    def __init__(self, channel, title, subtitle, description, start_time, stop_time, duration, episode_num=None, series=False, categories = []):
         self.title = title
         self.subtitle = subtitle
-#        if pub_date is None:
- #           pub_date = datetime.utcnow()
         self.description = description
-        self.startTime = start_time
-        self.duration = duration
+        self.start_time = start_time
+        self.stop_time = stop_time
         self.channel = channel
+        self.duration = duration
+        self.episode_num = episode_num
         self.series = series
         self.categories = self.mapToCategory(categories)
         #self.channel_id = channel.id
@@ -68,60 +68,90 @@ class Programme(db.Model):
     @staticmethod
     def with_title(title):
         return Programme.query.\
-            filter(Programme.startTime > datetime.now()).\
+            filter(Programme.start_time > datetime.now()).\
             filter(Programme.title == title).\
-            order_by(Programme.startTime).\
+            order_by(Programme.start_time).\
             all()
 
     @staticmethod
     def on_channel_with_title(channel, title):
         return Programme.query.\
-            filter(Programme.startTime > datetime.now()).\
+            filter(Programme.start_time > datetime.now()).\
             filter(Programme.title == title).\
             filter(Programme.channel==channel).\
-            order_by(Programme.startTime).\
+            order_by(Programme.start_time).\
             all()
+
+    @staticmethod
+    def on_channel_with_title_and_start_time(channel, title, start_time):
+        return Programme.query.\
+            filter(Programme.channel == channel).\
+            filter(Programme.title == title).\
+            filter(Programme.start_time.between((start_time - timedelta(minutes=1)), (start_time + timedelta(minutes=1))))\
+            .all()
 
     @staticmethod
     def titles_containing(text):
         return Programme.query.\
             filter(Programme.title.like('%' + text + '%')).\
-            filter(Programme.startTime > datetime.now()).\
+            filter(Programme.start_time > datetime.now()).\
             order_by(Programme.title).\
-            limit(8)
+            limit(8).all()
 
     @staticmethod
     def with_title_containing(text):
         return Programme.query.\
-            filter(Programme.startTime > datetime.now()).\
+            filter(Programme.start_time > datetime.now()).\
             filter(Programme.title.like('%' + text + '%')).\
-            order_by(Programme.startTime).\
-            limit(20)
+            order_by(Programme.start_time).\
+            limit(20).all()
+
+    @staticmethod
+    def _current_programme_for(channel, now):
+        from .master_import import json_friendly_tuple
+        if not channel.hidden:
+            #(Programme.id, Programme.title, Programme.startTime, Programme.duration)
+            programme = db.session.query(Programme.id, Programme.title, Programme.start_time, Programme.duration).\
+            filter(Programme.channel == channel).\
+            filter(Programme.start_time <= now).\
+            filter(Programme.stop_time > now).\
+            first()
+            return json_friendly_tuple(programme)
+
+    @staticmethod
+    def _upcoming_programmes_for(channel, limit, now):
+        from .master_import import json_friendly_tuple
+        if channel.hidden:
+            return []
+        else:
+            results = db.session.query(Programme.id, Programme.title, Programme.start_time, Programme.duration).filter(Programme.channel == channel).filter(Programme.start_time > now).order_by(Programme.start_time).limit(limit).all()
+
+            serialized_list = []
+            for programme in results:
+                serialized_list.append(json_friendly_tuple(programme))
+
+            return serialized_list
+
 
     def __repr__(self):
         return '<Programme serialized: %s>' % (self.serialize)
+
+
 
     @property
     def serialize(self):
         from .master_import import safe_value
         """Return object data in easily serializeable format"""
+
         return {
             'id'   : self.id,
             'title': safe_value(self.title),
             'subtitle'  : safe_value(self.subtitle),
             'description'  : safe_value(self.description),
-            'startTime'  : self.startTime,
+            'start_time'  : self.start_time.isoformat(),
+            'startTime'  : self.start_time.isoformat(),
+            'stop_time'  : self.stop_time.isoformat(),
             'duration'  : self.duration,
             'channel'  : self.channel.serialize if self.channel is not None else None
         }
 
-    id = db.Column(Integer, primary_key=True)
-    title = db.Column(String(255))
-    subtitle = db.Column(String(255))
-    description = db.Column(db.Text)
-    startTime = db.Column(db.DateTime)
-    duration = db.Column(Integer)
-
-    channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'))
-    channel = db.relationship('Channel', primaryjoin="Programme.channel_id == Channel.id",
-                              backref=db.backref('programmes', lazy='dynamic'))
