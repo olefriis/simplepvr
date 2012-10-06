220eaafd6df4ecd221cc9bcd0fe62e03e33d4647
Flemming Jønsson <flemming@joensson.org>
2012-10-06 21:37:54 +0200

    Error handling fixed to make the script fail when import is not successful. DateTimes from EPG file converted into system timezone before import to allow for handling EPG data from both ontv.dk and epgs.com

--- python/read_xmltv.py
+++ python/read_xmltv.py
@@ -5,6 +5,7 @@ import sys
 #from simple_pvr.pvr_initializer import setup
 from simple_pvr.xmltv_reader import XmltvReader
 from simple_pvr.database_initializer import *
+from simple_pvr.pvr_logger import logger
 import time
 
 db.create_all()
@@ -15,16 +16,20 @@ if len(sys.argv) != 3:
 
 ##Fixme - do we need setup?
 #setup()
+try:
+    stream = file(sys.argv[2], 'r')
+    mapping_to_channels = yaml.load(stream)
 
-stream = file(sys.argv[2], 'r')
-mapping_to_channels = yaml.load(stream)
+    reader = XmltvReader(mapping_to_channels)
 
-reader = XmltvReader(mapping_to_channels)
+    xmltv_file = sys.argv[1]
 
-xmltv_file = sys.argv[1]
+    start_time = time.time()
+    reader.read(xmltv_file)
+    end_time = time.time()
 
-start_time = time.time()
-reader.read(xmltv_file)
-end_time = time.time()
-
-print "Imported EPG in {} seconds".format((end_time-start_time))
+    print "Imported EPG in {} seconds".format((end_time-start_time))
+    sys.exit(0)
+except Exception, err:
+    logger().error("Failed importing EPG data - reason {}".format(str(err)))
+    sys.exit(1)
