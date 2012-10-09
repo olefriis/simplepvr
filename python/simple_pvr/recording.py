09ae9f05fdb0409ff618228f251a6021249b0c86
Flemming Jønsson <flemming@joensson.org>
2012-10-09 08:33:27 +0200

    Add stop_time to Recording

--- python/simple_pvr/recording.py
+++ python/simple_pvr/recording.py
@@ -7,13 +7,15 @@ class Recording:
     channel = None
     show_name = None
     start_time = None
+    stop_time = None
     duration = None
     programme = None
 
-    def __init__(self, channel, show_name, start_time, duration, programme):
+    def __init__(self, channel, show_name, start_time, stop_time, duration, programme):
         self.channel = channel
         self.show_name = show_name
         self.start_time = start_time
+        self.stop_time = stop_time
         self.duration = duration
         self.programme = programme
 
@@ -24,7 +26,8 @@ class Recording:
         return self.expired_at(datetime.now()) #(self.start_time + timedelta(seconds = self.duration)) < datetime.now()
 
     def expired_at(self, time):
-        return self._end_time() < time
+        #return self._end_time() < time
+        return self.stop_time <= time
 
     def inspect(self):
         return "'{0}' from '{1}' at '{2}'".format(self.show_name, self.channel, self.start_time)
@@ -37,7 +40,26 @@ class Recording:
         return (self.start_time - other.start_time).total_seconds()
 
     def __hash__(self):
-        return hash((self.show_name, self.start_time, self.programme))
+        return hash((self.show_name, self.start_time, self.stop_time, self.programme))
 
     def _end_time(self):
         return self.start_time + timedelta(seconds = self.duration)
+
+    def __repr__(self):
+        return '<Recording serialized: %s>' % (self.serialize)
+
+
+    @property
+    def serialize(self):
+        from .master_import import safe_value
+        from dateutil.tz import tzlocal
+        """Return object data in easily serializeable format"""
+
+        return {
+            'show_name': safe_value(self.show_name),
+            'channel'  : self.channel.serialize if self.channel else None,
+            'start_time'  : self.start_time.replace(tzinfo=tzlocal()).isoformat() if self.start_time else None,
+            'stop_time'  : self.stop_time.replace(tzinfo=tzlocal()).isoformat() if self.stop_time else None,
+            'duration'  : self.duration,
+            'programme'  : self.programme.serialize if self.programme else None
+        }
