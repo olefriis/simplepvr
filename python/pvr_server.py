25c3d0296f623fdc652bdc7f0c39904d9ec5e370
Flemming Jønsson <flemming@joensson.org>
2012-10-09 08:07:44 +0200

    Bootstrap the instance of RecordingPlanner instead of server controlled lazy init

--- python/pvr_server.py
+++ python/pvr_server.py
@@ -1,6 +1,6 @@
 import sys
-from simple_pvr.pvr_initializer import setup, recording_manager
-from simple_pvr import RecordingManager
+from simple_pvr.pvr_initializer import setup
+from simple_pvr import RecordingPlanner
 from simple_pvr.database_initializer import *
 
 def main(argv=None):
@@ -12,7 +12,10 @@ def main(argv=None):
     print "Server running on main thread"
     from simple_pvr import server
     server.startServer()
+    server.recording_planner = RecordingPlanner()
+
     server.reload_schedules()
+
 
 if __name__ == "__main__":
     sys.exit(main())
